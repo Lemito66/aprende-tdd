ab6c9f2deedd75f1af4bd515765b9ed21d483a3e
Emill <lemito66@gmail.com>
2023-02-27 10:58:52 -0500

    Creando pruebas de fizzbuzz

---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+
+const fizzbuzz = () => {
+
+}
+
+
+describe('fizzbuzz', () =>{
+    it('should be a function', () => {
+        expect(typeof fizzbuzz).toBe('function');
+    });
+});
