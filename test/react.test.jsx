4101a01518a981128d996bc369ba07b2ef5be628
Emill <lemito66@gmail.com>
2023-02-28 17:42:04 -0500

    pruebas con texbox

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,8 +1,35 @@
 import { render, screen, cleanup } from "@testing-library/react";
-import { afterEach, describe, it } from "vitest";
+import { afterEach, describe, it, expect } from "vitest";
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+
+const operations = ["+", "-", "*", "/"];
+
+const equalSign = "=";
 
 const Calculator = () => {
-  return <h1>Calculator</h1>;
+  return (
+    <section>
+      <h1>Calculator</h1>
+      <div role="grid">
+        {rows.map((row, idx) => (
+          <div role="row" key={idx}>
+            {row.map((number) => (
+              <span key={number}>{number}</span>
+            ))}
+          </div>
+        ))}
+
+        {operations.map((operation) => (
+          <span key={operation}>{operation}</span>
+        ))}
+
+        <span>{equalSign}</span>
+      </div>
+    </section>
+  );
 };
 
 describe("Calculator", () => {
@@ -17,4 +44,38 @@ describe("Calculator", () => {
 
     screen.getByText("Calculator");
   });
+
+  it("should render numbers", () => {
+    render(<Calculator />);
+
+    numbers.forEach((number) => {
+      screen.getByText(number);
+    });
+  });
+
+  it("should render 4 rows", () => {
+    render(<Calculator />);
+
+    const rows = screen.getAllByRole("row");
+    expect(rows.length).toBe(4);
+  });
+
+  it("should render operations", () => {
+    render(<Calculator />);
+
+    operations.forEach((operation) => {
+      screen.getByText(operation);
+    });
+  });
+
+  it("should render equals", () => {
+    render(<Calculator />);
+
+    screen.getByText("=");
+  });
+
+  it('should render an input',() => {
+    render(<Calculator />);
+    screen.getByRole('textbox')
+  })
 });
